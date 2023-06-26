6563a6ec7da1ed4a36cc16945cc10b5effbc3704
Ashu <abhishekaggarwal2801@gmail.com>
2023-06-26 15:51:35 +0530

    transactions of a client

--- server/controllers/Client.js
+++ server/controllers/Client.js
@@ -1,4 +1,5 @@
 const Client = require("../models/Client");
+const Transaction = require("../models/Transaction");
 
 //Additon of new client
 
@@ -32,7 +33,7 @@ exports.createClient = async (req, res) => {
   }
 };
 
-//Function for fetching all transaction of client
+//Function for fetching all orders of client
 exports.getAllOrders = async (req, res) => {
   try {
     //Fetch data from req
@@ -62,6 +63,46 @@ exports.getAllOrders = async (req, res) => {
       success: true,
       message: "Orders fetch successfully",
       ordersDetail,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Error",
+    });
+  }
+};
+
+// function for fetching all deliveries of a client
+exports.getAllTransactions = async (req, res) => {
+  try {
+    //Fetch data from req
+    const { firmName } = req.body;
+
+    //Validation
+    if (!firmName) {
+      return res.status(400).json({
+        success: false,
+        message: "Please Select Firm ",
+      });
+    }
+
+    const clientEntity = await Client.findOne({ firmName: firmName });
+
+    const transactionDetails = await Transaction.find({
+      client: clientEntity._id,
+    });
+
+    if (!transactionDetails) {
+      return res.status(401).json({
+        success: false,
+        message: "No Transaction found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Orders fetch successfully",
+      transactionDetails,
     });
   } catch (error) {
     return res.status(500).json({
--- server/routes/Client.js
+++ server/routes/Client.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-
-const {createClient, getAllOrders} = require('../controllers/Client');
+const {
+  createClient,
+  getAllOrders,
+  getAllTransactions,
+} = require("../controllers/Client");
 
 router.post("/createClient", createClient);
 router.get("/getAllOrders", getAllOrders);
+router.get("/getAllTransactions", getAllTransactions);
 
 module.exports = router;
